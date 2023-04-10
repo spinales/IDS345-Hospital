8073c09788129cc9f3b91005ac5d91024318b00b
David <122846601+QuezadaPD@users.noreply.github.com>
2023-04-10 11:54:35 -0400

    Cambios en appCaja

--- Caja/appCaja.Designer.cs
+++ Caja/appCaja.Designer.cs
@@ -30,31 +30,31 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(appCaja));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pbFacturacion = new System.Windows.Forms.PictureBox();
+            this.pbCuenta = new System.Windows.Forms.PictureBox();
+            this.pbCuadre = new System.Windows.Forms.PictureBox();
+            this.pbPerfil = new System.Windows.Forms.PictureBox();
             this.btnFacturacion = new System.Windows.Forms.Button();
             this.btnCuenta = new System.Windows.Forms.Button();
             this.btnCuadre = new System.Windows.Forms.Button();
             this.btnPerfil = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.pfLoader = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFacturacion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuadre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPerfil)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.pictureBox4);
-            this.panel1.Controls.Add(this.pictureBox3);
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pbFacturacion);
+            this.panel1.Controls.Add(this.pbCuenta);
+            this.panel1.Controls.Add(this.pbCuadre);
+            this.panel1.Controls.Add(this.pbPerfil);
             this.panel1.Controls.Add(this.btnFacturacion);
             this.panel1.Controls.Add(this.btnCuenta);
             this.panel1.Controls.Add(this.btnCuadre);
@@ -64,6 +64,47 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(265, 1024);
             this.panel1.TabIndex = 0;
+            // 
+            // pbFacturacion
+            // 
+            this.pbFacturacion.BackColor = System.Drawing.Color.White;
+            this.pbFacturacion.Image = ((System.Drawing.Image)(resources.GetObject("pbFacturacion.Image")));
+            this.pbFacturacion.Location = new System.Drawing.Point(71, 285);
+            this.pbFacturacion.Name = "pbFacturacion";
+            this.pbFacturacion.Size = new System.Drawing.Size(22, 22);
+            this.pbFacturacion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFacturacion.TabIndex = 5;
+            this.pbFacturacion.TabStop = false;
+            // 
+            // pbCuenta
+            // 
+            this.pbCuenta.Image = ((System.Drawing.Image)(resources.GetObject("pbCuenta.Image")));
+            this.pbCuenta.Location = new System.Drawing.Point(71, 244);
+            this.pbCuenta.Name = "pbCuenta";
+            this.pbCuenta.Size = new System.Drawing.Size(22, 22);
+            this.pbCuenta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCuenta.TabIndex = 4;
+            this.pbCuenta.TabStop = false;
+            // 
+            // pbCuadre
+            // 
+            this.pbCuadre.Image = ((System.Drawing.Image)(resources.GetObject("pbCuadre.Image")));
+            this.pbCuadre.Location = new System.Drawing.Point(71, 202);
+            this.pbCuadre.Name = "pbCuadre";
+            this.pbCuadre.Size = new System.Drawing.Size(22, 22);
+            this.pbCuadre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCuadre.TabIndex = 3;
+            this.pbCuadre.TabStop = false;
+            // 
+            // pbPerfil
+            // 
+            this.pbPerfil.Image = ((System.Drawing.Image)(resources.GetObject("pbPerfil.Image")));
+            this.pbPerfil.Location = new System.Drawing.Point(71, 163);
+            this.pbPerfil.Name = "pbPerfil";
+            this.pbPerfil.Size = new System.Drawing.Size(22, 22);
+            this.pbPerfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPerfil.TabIndex = 2;
+            this.pbPerfil.TabStop = false;
             // 
             // btnFacturacion
             // 
@@ -135,6 +176,15 @@
             this.btnPerfil.Click += new System.EventHandler(this.btnPerfil_Click);
             this.btnPerfil.Leave += new System.EventHandler(this.btnPerfil_Leave);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(265, 149);
+            this.panel2.TabIndex = 0;
+            // 
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -157,55 +207,6 @@
             this.pfLoader.Size = new System.Drawing.Size(1176, 1024);
             this.pfLoader.TabIndex = 7;
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(265, 149);
-            this.panel2.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(71, 163);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(22, 22);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(71, 202);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(22, 22);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(71, 244);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(22, 22);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 4;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(71, 285);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(22, 22);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 5;
-            this.pictureBox4.TabStop = false;
-            // 
             // appCaja
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
@@ -222,10 +223,10 @@
             this.Load += new System.EventHandler(this.appCaja_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFacturacion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuadre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPerfil)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -239,10 +240,10 @@
         private System.Windows.Forms.Button btnPerfil;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel pfLoader;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbPerfil;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pbFacturacion;
+        private System.Windows.Forms.PictureBox pbCuenta;
+        private System.Windows.Forms.PictureBox pbCuadre;
     }
 }
